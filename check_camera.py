e42149b5af27bc67e34991b7897fa8766e86d6d4
youngtree <ytree393@gmail.com>
2024-05-12 11:48:21 +0800

    v-0.2 YOLO & FPS

--- check_camera.py
+++ check_camera.py
@@ -1,15 +1,17 @@
 import cv2
 
+
 def main():
     num_cameras = 4  # 假设最多有8个相机连接到计算机上
 
-    for i in range(1, num_cameras+1):
+    for i in range(1, num_cameras + 1):
         cap = cv2.VideoCapture(i)
         if cap is not None and cap.isOpened():
             print(f"相机 {i} 可用")
             cap.release()
         else:
             print(f"相机 {i} 不可用")
+
 
 if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import cv2
+from ultralytics import YOLO
+import time
 
 
 def main():
+    # Load the YOLOv8 model
+    model = YOLO(r'D:\workingSpace\Projects\train240502\runs\detect\train\weights\best.pt')
     # 打开四个相机
-    num_cameras = 3
-    caps = [cv2.VideoCapture(i+1) for i in range(num_cameras)]
+    num_cameras = 4
+    caps = [cv2.VideoCapture(i) for i in range(1, num_cameras + 1)]
 
     # 设置相机分辨率为1920x1080
     for cap in caps:
@@ -12,14 +16,27 @@ def main():
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
     # 检查相机是否成功打开
-    for i, cap in enumerate(caps):
+    for i, cap in enumerate(caps, start=1):
         if not cap.isOpened():
             print(f"无法打开相机 {i}")
             return
 
+    # 初始化时间
+    prev_time = time.time()
     while True:
+        # 计算实时 FPS
+        current_time = time.time()
+        elapsed_time = current_time - prev_time
+        prev_time = current_time
+
+        # 避免除以零错误
+        if elapsed_time != 0:
+            fps = 1 / elapsed_time
+        else:
+            fps = 0
+
         # 逐个相机捕获视频并显示
-        for i, cap in enumerate(caps):
+        for i, cap in enumerate(caps, start=1):
             ret, frame = cap.read()
             if not ret:
                 print(f"无法读取相机 {i} 的视频帧")
@@ -27,9 +44,15 @@ def main():
 
             # 缩小图像以适应屏幕大小
             resized_frame = cv2.resize(frame, (480, 270))
+            results = model(resized_frame)
+            # Visualize the results on the frame
+            annotated_frame = results[0].plot()
+
+            # 在帧上绘制实时 FPS
+            cv2.putText(annotated_frame, f"FPS: {int(fps)}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
             # 显示缩小后的图像
-            cv2.imshow(f'Camera {i}', resized_frame)
+            cv2.imshow(f'Camera {i}', annotated_frame)
 
         # 检查是否按下了'q'键，如果是则退出循环
         if cv2.waitKey(1) & 0xFF == ord('q'):
